594182330fa14b21d5cdb704ad5ffceb0c325d9a
Vicktor Desrony <vldcreation21@gmail.com>
2024-05-31 21:28:35 +0700

    fix lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from faker import Faker
 
+
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful"""
