fac9a1da79e941e2f679f7760c06a1ac8b33d7ff
Vicktor Desrony <vldcreation21@gmail.com>
2024-05-31 21:10:12 +0700

    add user model tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,19 @@
+"""Test for models"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from faker import Faker
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        fake = Faker()
+        email = fake.email()
+        password = fake.password()
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
