b1ab51f124f44fd83093eb048d0fc351d913a473
Vicktor Desrony <vldcreation21@gmail.com>
2024-05-31 22:20:36 +0700

    add test normalize email addresses

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -18,3 +18,22 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        sampleTests = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+        ]
+
+        for email, expected in sampleTests:
+            user = get_user_model().objects.create_user(email, 'test123')
+
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
