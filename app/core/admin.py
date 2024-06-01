8ac0122cc4b5eb101fabee0a86facfef267b2eb4
Vicktor Desrony <vldcreation21@gmail.com>
2024-06-01 11:38:20 +0700

    add support creating and modifying users

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _ # as _ is a convention for translation
 
 from core import models
 
@@ -10,6 +11,35 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        ('Detail User', {'fields': ('email', 'password')}),  # None is the title of the section
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ('last_login',)  # Fields that are read only
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
